a5c6727a09d530fb2a37b81649f52c7ef546ff82
elizajasin <eliza_jasin@hotmail.com>
2016-04-03 15:24:17 +0700

    finish

--- pseudoColor.m
+++ pseudoColor.m
@@ -3,74 +3,50 @@
 % Date: 1 April 2016
 % Author: Eliza Riviera Rachmawati Jasin - 1103130144
 
-y = imresize(imread('gigi.jpg'),[256,256]); 
-%y=rgb2gray(img);
-[p,q,r]=size(y);
+% Load image grayscale
+img = imread('rusuk256.jpg'); 
+
+% Ubah ke RGB
+[p,q,r]=size(img);
 for i=1:1:p
     for j=1:1:q
-        if (y(i,j)>= 0) && (y(i,j)< 18)
-            x(i,j,1)=0;
-            x(i,j,2)=0;
-            x(i,j,3)=0;
-        elseif (y(i,j)>= 18) && (y(i,j)< 36)
-            x(i,j,1)=237;
-            x(i,j,2)=27;
-            x(i,j,3)=36;
-        elseif (y(i,j)>= 36) && (y(i,j)< 54)
-            x(i,j,1)=228;
-            x(i,j,2)=142;
-            x(i,j,3)=31;
-        elseif (y(i,j)>= 54) && (y(i,j)< 72)
-            x(i,j,1)=251;
-            x(i,j,2)=179;
-            x(i,j,3)=180;
-        elseif (y(i,j)>= 72) && (y(i,j)< 90)
-            x(i,j,1)=21;
-            x(i,j,2)=154;
-            x(i,j,3)=233;    
-        elseif (y(i,j)>= 90) && (y(i,j)< 108)
-            x(i,j,1)=116;
-            x(i,j,2)=3;
-            x(i,j,3)=59;
-        elseif (y(i,j)>= 108) && (y(i,j)< 126)
+        if (img(i,j)>= 0) && (img(i,j)< 32)
+            x(i,j,1)=5;
+            x(i,j,2)=8;
+            x(i,j,3)=45;
+        elseif (img(i,j)>= 32) && (img(i,j)< 64)
+            x(i,j,1)=27;
+            x(i,j,2)=227;
+            x(i,j,3)=38;
+        elseif (img(i,j)>= 64) && (img(i,j)< 96)
+            x(i,j,1)=128;
+            x(i,j,2)=102;
+            x(i,j,3)=255;
+        elseif (img(i,j)>= 96) && (img(i,j)< 128)
+            x(i,j,1)=200;
+            x(i,j,2)=119;
+            x(i,j,3)=189;
+        elseif (img(i,j)>= 128) && (img(i,j)< 160)
+            x(i,j,1)=37;
+            x(i,j,2)=104;
+            x(i,j,3)=237;    
+        elseif (img(i,j)>= 160) && (img(i,j)< 192)
+            x(i,j,1)=1;
+            x(i,j,2)=70;
+            x(i,j,3)=49;
+        elseif (img(i,j)>= 192) && (img(i,j)< 224)
             x(i,j,1)=252;
-            x(i,j,2)=234;
-            x(i,j,3)=12;
-        elseif (y(i,j)>= 126) && (y(i,j)< 144)
-             x(i,j,1)=146;
-            x(i,j,2)=80;
-            x(i,j,3)=167;
-        elseif (y(i,j)>= 144) && (y(i,j)< 162)
-            x(i,j,1)=203;
-            x(i,j,2)=213;
-            x(i,j,3)=62;
-        elseif (y(i,j)>= 162) && (y(i,j)< 180)
-            x(i,j,1)=59;
-            x(i,j,2)=165;
-            x(i,j,3)=77;
-        elseif (y(i,j)>= 180) && (y(i,j)< 198)
-            x(i,j,1)=48;
-            x(i,j,2)=85;
-            x(i,j,3)=173;
-        elseif (y(i,j)>= 198) && (y(i,j)< 216)
-            x(i,j,1)=126;
+            x(i,j,2)=224;
+            x(i,j,3)=127;
+        elseif (img(i,j)>= 224) && (img(i,j)< 255)
+             x(i,j,1)=16;
             x(i,j,2)=180;
-            x(i,j,3)=67;
-        elseif (y(i,j)>= 216) && (y(i,j)< 232)
-            x(i,j,1)=16;
-            x(i,j,2)=233;
-            x(i,j,3)=59;
-        elseif (y(i,j)>= 232) && (y(i,j)< 255)
-            x(i,j,1)=255;
-            x(i,j,2)=255;
-            x(i,j,3)=100;
+            x(i,j,3)=237;
         end
     end
 end
-x=x/255;
-P = x;
+imgRGB=x/255;
 
-figure();
-imshow(y);
-figure();
-imshow(x);
+% Tampilkan gambar
+subplot(1,2,1); imshow(img); title('Image asli');
+subplot(1,2,2); imshow(imgRGB); title('Image RGB');
